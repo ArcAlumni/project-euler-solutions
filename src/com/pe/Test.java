a64150bfccb702ce6cb2e6a21f733accfb00dc4c
priyan <priyanpaulraj@gmail.com>
2021-11-26 20:43:34 +0530

    modified tests

--- src/com/pe/Test.java
+++ src/com/pe/Test.java
@@ -1,11 +1,6 @@
 package com.pe;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
