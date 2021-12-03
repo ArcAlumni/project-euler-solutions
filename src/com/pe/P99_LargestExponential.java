4d0157b5bc7ef6e144f69e90ec90130231b7a75d
priyan <priyanpaulraj@gmail.com>
2021-12-03 13:05:58 +0530

    p99

---
+++ src/com/pe/P99_LargestExponential.java
@@ -0,0 +1,17 @@
+package com.pe;
+
+public class P99_LargestExponential implements Solution {
+
+    public static void main(String[] args) throws Exception {
+        new P99_LargestExponential().solve();
+    }
+
+    @Override
+    public String solve() throws Exception {
+
+        //System.out.println(pow("519432", 525806));
+
+        return null;
+    }
+
+}
